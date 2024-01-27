ccc8ce8750ea64035f49837aca294a5c2f5814aa
Sumit Suryawanshi <suryawanshisumit2003@gmail.com>
2024-01-27 16:01:54 +0530

    Added server

--- server/main.py
+++ server/main.py
@@ -1,5 +1,7 @@
 from typing import Union
 
+import uvicorn
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -13,3 +15,12 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        app,
+        host="127.0.0.2",
+        port=8081
+    )
