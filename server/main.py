1319cf6fb3d39f48a44cb449a9a13ce1c5d47367
Sumit Suryawanshi <suryawanshisumit2003@gmail.com>
2024-01-27 18:23:35 +0530

    Added resume api

--- server/main.py
+++ server/main.py
@@ -1,26 +1,58 @@
-from typing import Union
 
-import uvicorn
+from fastapi import FastAPI, File, UploadFile, HTTPException, Form
+import google.generativeai as genai
+import os
+import PyPDF2 as pdf
+from dotenv import load_dotenv
 
-from fastapi import FastAPI
+load_dotenv()
 
 app = FastAPI()
 
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def get_gemini_response(input):
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content(input)
+    return response.text
 
+
+def input_pdf_text(uploaded_file):
+    reader = pdf.PdfReader(uploaded_file.file)
+    text = ""
+    for page in range(len(reader.pages)):
+        page = reader.pages[page]
+        text += str(page.extract_text())
+    return text
+
+
+input_prompt = """
+Hey Act Like a skilled or very experience ATS(Application Tracking System)
+with a deep understanding of tech field, software engineering, data science, data analyst
+and big data engineer. Your task is to evaluate the resume based on the given job description.
+You must consider the job market is very competitive and you should provide 
+best assistance for improving the resumes. Assign the percentage Matching based 
+on JD and
+the missing keywords with high accuracy
+resume:{text}
+description:{jd}
+
+I want the response in one single string having the structure
+{{"JD Match":"%","MissingKeywords":[], "Profile Summary":""}}
+"""
+
+
+@app.post("/Resume_analyser/")
+async def submit_resume(jd: str = Form(...), resume: UploadFile = File(...)):
+    text = input_pdf_text(resume)
+    response = get_gemini_response(input_prompt.format(text=text, jd=jd))
+    return {"response": response}
 
 
 if __name__ == "__main__":
-    uvicorn.run(
-        app,
-        host="127.0.0.2",
-        port=8081
-    )
+    import uvicorn
+
+    uvicorn.run(app, host="127.0.0.1", port=8081)
+
+
